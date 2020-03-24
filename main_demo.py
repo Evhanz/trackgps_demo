27f88cf5b16b4b5785c9f73233d74ae81dee43a1
Eidelman <eidelman.hernandez@services.mss.pe>
2020-03-24 16:48:18 -0500

    wip

--- main_demo.py
+++ main_demo.py
@@ -1,13 +1,15 @@
 import requests
 def main():
-  url = 'http://www.httpbin.org/get'
+  url = 'http://trackingmss.server93.com/comGpsGate/api/v.1/applications/4/tokens'
   args = {'nombre':'Eidel', 'curso':'py'}
+  json = {"password": "Mss$2045", "username": "admin"}
   try:
-      response = requests.get(url, params=args)
+      response = requests.post(url, json=json)
       if response.status_code == 200:
           response_json = response.json()
-          origin = response_json['args']
-          print(origin['curso'])
+          print(response_json['token'])
+          # origin = response_json['args']
+          # print(origin['curso'])
         #  print(response_json)
         #   Tipo 1  
         #   file = open('google.html', 'wb')
--- repository.py
+++ repository.py
@@ -1,3 +1,42 @@
 #Esta libreria se encarga de traer la data
-server = 
-apiGetPoints = 
+import time
+import requests
+
+#variables globales
+server = "http://trackingmss.server93.com/comGpsGate/api/v.1"
+
+
+def getToken(idAplication = 4):
+    global server
+    url = "{server}/applications/{idAplication}/tokens"
+    json = {"password": "Mss$2045", "username": "admin"}
+    token = ""
+    try:
+        response = requests.post(url, json=json)
+        if response.status_code == 200:
+            response_json = response.json()
+            token = response_json['token']
+    except:
+            print("La url no existe , o no es accesible")
+    return token
+
+
+def getPoints(idAplication = 4 , idDevice = 10, fecha = None, timeRange = []):
+    #la comuniacion es por get : revisar la documentacion
+    dateNow = time.strftime("%d/%m/%y")
+    data = []
+
+    if fecha is None:
+        fecha = dateNow
+    if len(timeRange) !=2:
+        timeRange[0]=   
+
+    apiGetPoints = "/applications/{idAplication}/users/{idDevice}/tracks?Date={fecha}&From={timeRange[0]}&Until={timeRange[1]}Filtered=true"
+    token = getToken();
+
+    response = requests.get(apiGetPoints,headers={"Authorization": token})
+    if response.status_code == 200:
+        data = response.json()
+
+    return data
+            
